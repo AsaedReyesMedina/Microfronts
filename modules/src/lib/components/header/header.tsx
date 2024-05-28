8a7ae7248cf5087d6b981ba5faa3d5c1ac8a5f60
AsaedReyesMedina <asaed.reyes@kubofinanciero.com>
2024-05-27 19:54:41 -0600

    feat: :sparkles: update kds

--- modules/src/lib/components/header/header.tsx
+++ modules/src/lib/components/header/header.tsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Button, MantineProvider } from '@mantine/core';
+import { Button } from '@kds/kdsr';
 import './app.css';
 export function Header() {
   return (
     <>
-      <MantineProvider>
-        <Button>Components header</Button>
-      </MantineProvider>
+      <Button label='client-item' variant='contained' size='small' />
     </>
 
   );
--- modules/src/lib/solicitudes/client-item/clientitem.tsx
+++ modules/src/lib/solicitudes/client-item/clientitem.tsx
@@ -1,13 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Button, MantineProvider } from '@mantine/core';
+
+import { Button } from '@kds/kdsr';
 import './app.css';
 export function ClientItem() {
 
   return (
     <>
-      <MantineProvider>
-        <Button>solicitudes client item</Button>
-      </MantineProvider>
+      <Button label='client-item' variant='contained' size='small' />
     </>
   );
 }
